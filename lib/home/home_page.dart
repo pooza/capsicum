e7d33f59204275892ab31cd699c56e3be2146421
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-10-13 03:42:51 +0900

    #4

---
+++ lib/home/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:capsicum/home/home_page_state.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
---
+++ lib/home/home_page_state.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:yaml/yaml.dart';
+import 'package:capsicum/home/home_page.dart';
+
+class HomePageState extends State<HomePage> {
+  String _title = 'untitled';
+  String _yaml = 'Load YAML Data';
+  var _pubspec = {};
+
+  void _loadPubspec() {
+    setState(() {
+      _updateTitle();
+    });
+  }
+
+  Future<void> _updateTitle() async {
+    _yaml = await rootBundle.loadString('config/pubspec.yaml');
+    _pubspec = loadYaml(_yaml);
+    _title = _pubspec['name'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget root = Scaffold(
+      appBar: AppBar(title: Text('capsicum')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loadPubspec,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+    return root;
+  }
+}
