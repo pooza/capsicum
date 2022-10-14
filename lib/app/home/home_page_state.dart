2ca47508ccf85b7fc57d6bfebed9584c667355f3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-10-14 12:27:56 +0900

    #7

--- lib/app/home/home_page_state.dart
+++ lib/app/home/home_page_state.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:capsicum/app/home/home_page.dart';
 import 'package:capsicum/utils/pubspec.dart';
 
 class HomePageState extends State<HomePage> {
-  Pubspec _pubspec = Pubspec();
+  final Pubspec _pubspec = Pubspec();
   String _title = 'untitled';
   String _version = '';
+  List<dynamic> _accounts = [];
 
   void loadPubspec() async {
     await _pubspec.load();
@@ -15,9 +18,23 @@ class HomePageState extends State<HomePage> {
     });
   }
 
+  void loadAccounts() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('accounts') == null) {
+      prefs.setString('accounts', '[]');
+    }
+    _accounts = await jsonDecode(prefs.getString('accounts') ?? '[]');
+  }
+
+  @override
+  void initState() {
+    loadPubspec();
+    loadAccounts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    loadPubspec();
     return Scaffold(
       appBar: AppBar(title: Text(_title)),
       body: Center(
