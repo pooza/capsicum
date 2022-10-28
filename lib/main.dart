41773b61c9dfe28d977e489cebbacbde88d11630
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-10-29 00:54:47 +0900

    #21

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:capsicum/app/home/home_page.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const Capsicum());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(const Capsicum());
+  });
+}
 
 class Capsicum extends StatelessWidget {
   const Capsicum({super.key});
